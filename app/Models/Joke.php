2aa8bb721eca8acbf15239c71428895af0f619b2
sasho.krist <akr@monizze.be>
2022-10-11 10:49:16 +0300

    test

---
+++ app/Models/Joke.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Orchid\Attachment\Attachable;
+use Orchid\Filters\Filterable;
+use Orchid\Screen\AsSource;
+
+class Joke extends Model
+{
+    use HasFactory, AsSource, Filterable, Attachable;
+
+    protected $fillable = [
+      'type',
+      'setup',
+      'punchline'
+    ];
+
+    /**
+     * The attributes for which you can use filters in url.
+     *
+     * @var array
+     */
+    protected $allowedFilters = [
+        'type',
+        'setup',
+        'punchline'
+    ];
+
+    /**
+     * The attributes for which can use sort in url.
+     *
+     * @var array
+     */
+    protected $allowedSorts = [
+        'type',
+        'setup',
+        'punchline',
+        'updated_at',
+        'created_at',
+    ];
+}
